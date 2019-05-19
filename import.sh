033a0af22ec75a176a47658bd3b1bcc13fec3d88
Jaap Mengers <jaapm@q42.nl>
2019-05-19 13:16:28 +0200

    Runs import on build

---
+++ import.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl https://spreadsheets.google.com/feeds/list/1fZn-ofB0OLaakrXOF93d6QIc_qGszlBujR9GsppWrv0/1/public/values\?alt\=json | jq '.feed.entry | map({id: (.["gsx$id"] | .["$t"]), lat: (.["gsx$lat"] | .["$t"] | tonumber), lng: (.["gsx$lng"] | .["$t"] | tonumber), body: (.["gsx$body"] | .["$t"])})' > src/challenges.json
